198b1e1e6543bcafd8928c2f34bbce37f7d6adac
Allan Caetano de Oliveira <allan.co@hotmail.com>
2019-12-10 16:02:15 -0300

    teste de conexao com o mongodb

---
+++ Controllers/TransactionsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Repositories.Entities;
+using WebApplication3.Repositories;
+
+namespace WebApplication3.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly ITransactionsRepository _transactionsRepository;
+
+        public TransactionsController(ITransactionsRepository transactionsRepository)
+        {
+            _transactionsRepository = transactionsRepository;
+        }
+                
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TransactionEntity obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            obj.IsDebit = false;
+
+            _transactionsRepository.Insert(obj);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
