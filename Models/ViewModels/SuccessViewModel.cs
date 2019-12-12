e22cbcaf275a3afb288d65e3b683c3405a39d1c8
Allan Caetano <allan.co@hotmail.com>
2019-12-12 00:19:22 -0300

    ajustes layout

--- Models/ViewModels/SuccessViewModel.cs
+++ Models/ViewModels/SuccessViewModel.cs
@@ -6,9 +6,7 @@ namespace WebApplication3.Models.ViewModels
     public class SuccessViewModel
     {
         public string Message { get; set; }
-        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Value { get; set; }
-        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Balance { get; set; }
     }
 }
